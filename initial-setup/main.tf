322e784b70dcb9fa429fd1a217a4b49428d5da08
Alex Contreras <alexcontreras.alxlabs@gmail.com>
2024-06-09 23:47:35 -0500

    add initial instance, sec group

---
+++ initial-setup/main.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "web" {
+  instance_type          = "t3.micro"
+  ami                    = data.aws_ami.this.id
+  user_data              = filebase64("scripts/user_data.sh")
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+}
+
+data "aws_ami" "this" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+data "aws_vpc" "this" {
+  cidr_block = "172.21.0.0/16"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.this.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
